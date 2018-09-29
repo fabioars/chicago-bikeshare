c1497e2aa41a318a5eb0c44e5624a3ea60b8f1b8
Fabio <fabioars@live.com>
2018-09-29 11:08:09 -0300

    Tarefa 8: Responda a seguinte questão

--- chicago_bikeshare.py
+++ chicago_bikeshare.py
@@ -172,7 +172,7 @@ input("Aperte Enter para continuar...")
 male, female = count_gender(data_list)
 print("\nTAREFA 8: Por que a condição a seguir é Falsa?")
 print("male + female == len(data_list):", male + female == len(data_list))
-answer = "Escreva sua resposta aqui."
+answer = "Porque existe registros em branco, sem gênero."
 print("resposta:", answer)
 
 # ------------ NÃO MUDE NENHUM CÓDIGO AQUI ------------
